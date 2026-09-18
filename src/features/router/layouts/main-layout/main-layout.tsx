b8aa2e0f24574be95d70b907c852bb8c361532db
UrPo2006 <romaibrahim1w@gmail.com>
2026-09-18 23:46:13 +0300

    update frist

--- src/features/router/layouts/main-layout/main-layout.tsx
+++ src/features/router/layouts/main-layout/main-layout.tsx
@@ -1,13 +1,18 @@
 import LanguageSwitcher from '@/shared/components/custom-ui/language-switcher/language-switcher';
+
 import { useTranslation } from 'react-i18next';
 import { Outlet } from 'react-router-dom';
-
+import { ArrowUpRight } from 'lucide-react';
+import { Button } from '@/shared/components/ui/button';
 const MainLayout = () => {
   const { t } = useTranslation();
   return (
-    <div>
+    <div className="flex min-h-screen flex-col items-center justify-center">
       <LanguageSwitcher />
       <h1>{t('welcome')}</h1>
+      <Button variant="secondary" icon={<ArrowUpRight />}>
+        Get Started
+      </Button>
       <Outlet />
     </div>
   );
